3e0132a90372ed88e28bec6e15a8a5aad2945ce3
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2019-12-18 20:29:56 +0800

    1.3.1

--- src/source.ts
+++ src/source.ts
@@ -3,5 +3,6 @@ import { symbol } from "./Symbol";
 export function source(): string {
   const content = (self as any)[symbol];
   if (!content) return "";
-  return `self.${symbol}=${content};self.${symbol}();`;
+  const __self__ = `var __self__=(typeof self)!=='undefined'?self:this`;
+  return `${__self__};__self__.${symbol}=${content};__self__.${symbol}();`;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const WrapperPlugin = require('./scripts/wrapper-webpack-plugin');
 
 module.exports = (webpackConf) => {
+  const __self__ = `var __self__=(typeof self)!=='undefined'?self:this`;
   webpackConf.plugins.push(new WrapperPlugin({
     test: /\.js$/,
-    header: 'self.__channeler__=function(){',
-    footer: '};self.__channeler__();',
+    header: `${__self__};__self__.__channeler__=function(){`,
+    footer: '};__self__.__channeler__();',
   }));
 }
